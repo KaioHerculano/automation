cf4b938b6d8eb9d7757cd7c032253e1cee8ccb9b
KaioHerculano <kaioherculano12@gmail.com>
2025-11-22 01:29:44 -0400

    feat: add .env.example

--- app/settings.py
+++ app/settings.py
@@ -94,13 +94,6 @@ DATABASES = {
         'PORT': os.environ.get('POSTGRES_PORT', '5432'),
     }
 }
-"""else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }"""
 
 
 # Password validation
